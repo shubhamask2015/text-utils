12dd0cbc1647108a6e5a58ea2a188dfc6858abfd
shubhamask2015 <shubhamask2015@gmail.com>
2022-03-07 18:17:33 +0530

    Updates

---
+++ precache-manifest.5234793c98d18b5484ac1a2ccf2c076f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c5d568d5a14d5996d77",
+    "url": "/text-utils/static/css/main.37f429a7.chunk.css"
+  },
+  {
+    "revision": "3c5d568d5a14d5996d77",
+    "url": "/text-utils/static/js/main.3c5d568d.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/text-utils/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/text-utils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "cc7693ea043117531dae",
+    "url": "/text-utils/static/js/runtime~main.cc7693ea.js"
+  },
+  {
+    "revision": "f59c53d6dda10748814d6efed3535fea",
+    "url": "/text-utils/index.html"
+  }
+];
